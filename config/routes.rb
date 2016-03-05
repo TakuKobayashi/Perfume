ad6941440835a8810bb72cc5cece0ed5143eae79
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2016-03-05 19:32:21 +0900

    active_adminを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   resource :top, controller: :top, only: [:index]
 
   resource :photo, controller: :photo, only: [] do
