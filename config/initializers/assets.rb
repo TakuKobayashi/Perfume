55b056e055b9f39d11c4706e54bf43b50d2d0df2
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2016-03-06 14:25:46 +0900

    jqueryとかもろもろ入れた

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,3 +10,5 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( common.css )
+Rails.application.config.assets.precompile += %w( widgets.js )
+Rails.application.config.assets.precompile += %w( jquery.min.js )
